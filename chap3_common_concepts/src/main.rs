450747bca8d3978542636f7c6aefca11027abe1e
dhany007 <dhanyaritonang@gmail.com>
2025-05-04 00:59:29 +0700

    shadowing

--- chap3_common_concepts/src/main.rs
+++ chap3_common_concepts/src/main.rs
@@ -18,6 +18,26 @@ fn main() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("THREE_HOURS_IN_SECONDS: {THREE_HOURS_IN_SECONDS}");
     const PHI: f32 = 3.14;
-    println!("PHI: {PHI}")
+    println!("PHI: {PHI}");
 
+    println!("Shadowing");
+    let x = 5;
+    let x = x + 1; // x = 6
+    {
+        let x = x * 2; // x = 12
+        println!("The value of x in inner scope is: {x}") // x = 12
+    }
+    println!("The value of x is: {x}"); // x = 6, because outer scope
+
+
+    // if not used `mut`, maka tipe data tidak dipermasalahkan
+    let spaces = "   ";
+    println!("spaces: {spaces}");
+    let spaces = spaces.len();
+    println!("spaces: {spaces}");
+
+    let mut name = "Jon";
+    println!("name: {name}");
+    // name = 10; // akan error karena data type tidak sama, expected `&str`, found integer
+    // println!("name: {name}");
 }
