283888ade422a927f38f29a90d09f19575b82eb6
dhany007 <dhanyaritonang@gmail.com>
2025-05-04 23:16:30 +0700

    chap3: init function

---
+++ chap3_function/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Function");
+    // uses snake case as the conventional style for function and variable names,
+    // in which all letters are lowercase and underscores separate words
+    another_function()
+}
+
+fn another_function(){
+    println!("Another function");
+}
