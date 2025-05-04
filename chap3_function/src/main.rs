929b19e7678512fb2f2d8d1987fe7041350284db
dhany007 <dhanyaritonang@gmail.com>
2025-05-04 23:24:48 +0700

    chap3: function statement and expression

--- chap3_function/src/main.rs
+++ chap3_function/src/main.rs
@@ -4,6 +4,15 @@ fn main() {
     // in which all letters are lowercase and underscores separate words
     another_function(10);
     print_labeled_measurement(10, 'h');
+
+    println!("Statements and Expressions");
+    // Statements are instructions that perform some action and do not return a value.
+    // Expressions evaluate to a resultant value. Let’s look at some examples.
+    let y = {
+        let x = 5;
+        x + 1
+    };
+    println!("The value of y is: {y}"); // y = 6
 }
 
 fn another_function(x :i32){
