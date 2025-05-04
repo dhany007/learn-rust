008c1df068422662f23478d81c57dfaf0a22fbbc
dhany007 <dhanyaritonang@gmail.com>
2025-05-04 23:19:50 +0700

    chap3: function parameter

--- chap3_function/src/main.rs
+++ chap3_function/src/main.rs
@@ -2,9 +2,14 @@ fn main() {
     println!("Function");
     // uses snake case as the conventional style for function and variable names,
     // in which all letters are lowercase and underscores separate words
-    another_function()
+    another_function(10);
+    print_labeled_measurement(10, 'h');
 }
 
-fn another_function(){
-    println!("Another function");
+fn another_function(x :i32){
+    println!("The value of x is {x}");
+}
+
+fn print_labeled_measurement(x :i32, label :char) {
+    println!("The value of x is: {x}{label}")
 }
