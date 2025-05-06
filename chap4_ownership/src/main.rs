ecb34e184eda75b16dd25059c97bd03e305b1cf5
dhany007 <dhanyaritonang@gmail.com>
2025-05-06 11:27:11 +0700

    chap4: return value and scope

--- chap4_ownership/src/main.rs
+++ chap4_ownership/src/main.rs
@@ -32,7 +32,24 @@ fn main() {
     makes_copy(z); // because i32 implement the Copy trait,
                     // z does NOT move into this function
     println!("z = {z}"); // so it's okay z to move afterward
-} // here z goes out of scope then str. but because str's value was moved, nothing special happen
+
+    println!("Return values and scope");
+    let str1 = gives_ownership(); // gives_ownership moves its return value into str1
+    println!("str1 = {str1}");
+
+    let str2 = String::from("hello"); // str2 comes into this scope
+    println!("str2 = {str2}");
+
+    let str3 = takes_and_gives_back(str2); // str2 moved into  takes_and_gives_back,
+                                            // which is moves its return value into str3
+    println!("str3 = {str3}");
+
+    println!("return multiple values using a tuple");
+
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{s2}' is {len}.");
+}
 
 fn take_ownership(some_string: String) { // some_string comes into scope
     println!("{some_string}");
@@ -41,3 +58,19 @@ fn take_ownership(some_string: String) { // some_string comes into scope
 fn makes_copy(some_integer: i32){ // some_integer comes into scope
     println!("{some_integer}");
 } // here some_integer goes out scope. Nothing special happen
+
+fn gives_ownership() -> String { // give_ownership will move it's return value into the function who call it
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string // some_string is returned and moves out to the calling function
+}
+
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into scope
+    a_string // a_string is returned and moves out to the calling function
+}
+
+fn calculate_length(some_string: String) -> (String, usize) {
+    let length = some_string.len();
+
+    (some_string, length)
+}
