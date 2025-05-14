bcb8bac6222f6b29f920fbcba087063dbf525e4f
dhany007 <dhanyaritonang@gmail.com>
2025-05-14 14:30:33 +0700

    chap5: rectangle with struct

--- chap5_struct_example/src/main.rs
+++ chap5_struct_example/src/main.rs
@@ -13,6 +13,14 @@ fn main() {
     let rect1 = (30, 50);
     let result = area_tuples(rect1);
     println!("The area of the rectangle is {} square pixels", result);
+
+    println!("Refactoring with Structs: Adding More Meaning");
+    let rect2 = Rectangle{
+        width: 30,
+        height: 50,
+    };
+    let result = area_structs(&rect2);
+    println!("The area of the rectangle is {} square pixels", result);
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -22,4 +30,13 @@ fn area(width: u32, height: u32) -> u32 {
 // We would have to keep in mind that width is the tuple index 0 and height is the tuple index 1
 fn area_tuples(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area_structs(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
