687cac0e1e43da90518c226ebaa2a91b4ce1683a
dhany007 <dhanyaritonang@gmail.com>
2025-05-14 14:45:26 +0700

    chap5: rectangle debug derived

--- chap5_struct_example/src/main.rs
+++ chap5_struct_example/src/main.rs
@@ -21,6 +21,26 @@ fn main() {
     };
     let result = area_structs(&rect2);
     println!("The area of the rectangle is {} square pixels", result);
+
+    println!("Adding Useful Functionality with Derived Traits");
+    // use {:#?} for formatting print
+    // use #[derive(Debug)] in struct for debug
+    println!("rect2 is {rect2:#?}");
+
+    //  dbg!, which takes ownership of an expression (as opposed to println!, which takes a reference),
+    // prints the file and line number of where that dbg! macro call occurs in your code
+    // along with the resultant value of that expression, and returns ownership of the value.
+
+    let scale = 2;
+    let rect4 = Rectangle{
+        width: dbg!(30 * scale),
+        height: 50
+    };
+    dbg!(&rect4); // We don’t want dbg! to take ownership of rect1, so we use a reference to rect1 in the next call
+    // Note: Calling the dbg! macro prints to the standard error console stream (stderr),
+    // as opposed to println!, which prints to the standard output console stream (stdout)
+
+    // The dbg! macro can be really helpful when you’re trying to figure out what your code is doing!
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -31,7 +51,7 @@ fn area(width: u32, height: u32) -> u32 {
 fn area_tuples(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
 }
-
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
