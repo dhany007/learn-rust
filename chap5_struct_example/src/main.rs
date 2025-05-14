6a65dfabecbfc1365854cd84cc0932df7330a502
dhany007 <dhanyaritonang@gmail.com>
2025-05-14 14:14:28 +0700

    chap5: rectangle with tuples

--- chap5_struct_example/src/main.rs
+++ chap5_struct_example/src/main.rs
@@ -7,9 +7,19 @@ fn main() {
     let width1 = 30;
     let height1 = 50;
     let result = area(width1, height1);
-    println!("The area of the rectangle is {} square pixels", result)
+    println!("The area of the rectangle is {} square pixels", result);
+
+    println!("Refactoring with Tuples");
+    let rect1 = (30, 50);
+    let result = area_tuples(rect1);
+    println!("The area of the rectangle is {} square pixels", result);
 }
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+// We would have to keep in mind that width is the tuple index 0 and height is the tuple index 1
+fn area_tuples(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
