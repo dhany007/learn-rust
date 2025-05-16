4043c4e65738ff9a7bca42815ca942c5130851e7
dhany007 <dhanyaritonang@gmail.com>
2025-05-16 11:42:03 +0700

    chap5: summary

--- chap5_struct_method/src/main.rs
+++ chap5_struct_method/src/main.rs
@@ -38,6 +38,14 @@ fn main() {
     // like String::from("")
     let sq1 = Rectangle::square(30);
     println!("{:?}", sq1); // Rectangle { width: 30, height: 30 }
+
+    // Summary
+    // Structs let you create custom types that are meaningful for your domain.
+    // By using structs, you can keep associated pieces of data connected to each other
+    // and name each piece to make your code clear.
+    // In impl blocks, you can define functions that are associated with your type,
+    // and methods are a kind of associated function that let you specify the behavior
+    // that instances of your structs have.
 }
 
 #[derive(Debug)]
