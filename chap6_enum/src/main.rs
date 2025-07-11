085f69806a18aaaf1f1774880c5bfa1256b63986
dhany <dhany@citra-digital.org>
2025-07-11 10:39:45 +0700

    chap6: define enum

---
+++ chap6_enum/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    println!("Enum and Pattern Matching");
+
+    println!("Defining an Enum");
+    // enum value can only be one of its variants
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr{
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr{
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+}
+
+fn route(ip_kind: IpAddrKind) {
+
+}
+
+enum IpAddrKind {
+    V4,
+    V6
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String
+}
