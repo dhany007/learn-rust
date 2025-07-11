3414b5a40c4ec5bf0e0a5450dbea2b15a69fa48d
dhany <dhany@citra-digital.org>
2025-07-11 13:27:43 +0700

    chap6: match in enum coin

---
+++ chap6_pattern_matching/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("The Match Control Flow Construct!");
+    // match that allows you to compare a value against a series of patterns
+    // and then execute code based on which pattern matches.
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
