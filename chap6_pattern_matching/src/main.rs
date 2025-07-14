efc44f14fe2b8ba0501ae16e3d43803f9a58e99a
dhany <dhany@citra-digital.org>
2025-07-14 09:44:11 +0700

    chap6: action match does not bind value

--- chap6_pattern_matching/src/main.rs
+++ chap6_pattern_matching/src/main.rs
@@ -4,6 +4,13 @@ fn main() {
     // and then execute code based on which pattern matches.
     value_in_cents(Coin::Penny);
     value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    println!("Matching with Option<T>");
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("The value of six is {:?}", six);
+    let none = plus_one(None);
+    println!("The value of none is {:?}", none);
 }
 
 #[derive(Debug)]
@@ -32,5 +39,34 @@ fn value_in_cents(coin: Coin) -> u8 {
             println!("State quarter from {state:?}!");
             25
         },
+    }
+}
+
+/*
+    Let’s say we want to write a function that takes an Option<i32> and,
+    if there’s a value inside, adds 1 to that value.
+    If there isn’t a value inside, the function should return the None value
+    and not attempt to perform any operations.
+*/
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
+}
+
+/*
+    Using enums, we can also take special actions for a few particular values,
+    but for all other values take one default action
+    _ is a special pattern that matches any value and does not bind to that value
+*/
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn match_dice_roll(dice: u8) {
+    match dice {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (), // we don’t want to run any code in this case.
     }
 }
