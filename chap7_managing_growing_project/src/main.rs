c083e796f16dc6114f81fe2eec6dd64c55bf1adc
dhany <dhany@citra-digital.org>
2025-07-14 13:49:39 +0700

    chap7_declaring_modules

---
+++ chap7_managing_growing_project/src/main.rs
@@ -0,0 +1,13 @@
+use crate::garden::vegetables::Asparagus;
+
+pub mod garden; //include the code it finds in src/garden.rs
+
+fn main() {
+    println!("Managing Growing Projects with Packages, Crates and Modules!");
+    /*
+        - packages: a cargo feature that lets you build, test and share crates
+        - crates: a tree modules that produces a library or executable
+        - modules and use: let you control the organization, scope and privacy of paths
+        - paths: a way of naming an item, such as a struct, function or module
+     */
+}
