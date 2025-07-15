cb5a75fe313276ba846361ff11037bf33398df77
dhany <dhany@citra-digital.org>
2025-07-15 17:00:17 +0700

    chap9_error_handling_recoverable

--- chap9_error_handling/src/main.rs
+++ chap9_error_handling/src/main.rs
@@ -1,9 +1,21 @@
+use std::fs::File;
+
 fn main() {
     println!("Error Handling");
 
-    // unrecoverable errors with panic!
-    // panic!("crash and burn")
-    let  v = vec![1,2,3];
-    v[99]; // index out of bounds: the len is 3 but the index is 99
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("There was a problem opening the file: {:?}", error),
+    };
 }
 
+/*
+    T represents the type of the value that will be returned in a success case within the Ok variant,
+    and E represents the type of the error
+    that will be returned in a failure case within the Err variant
+ */
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
