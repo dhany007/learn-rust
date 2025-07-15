10113d19fbf7a7f60d6d620f0ccc05119982bfae
dhany <dhany@citra-digital.org>
2025-07-15 17:08:54 +0700

    chap9_error_handling_unwrap_or_else

--- chap9_error_handling/src/main.rs
+++ chap9_error_handling/src/main.rs
@@ -28,3 +28,16 @@ enum Result<T, E> {
     Ok(T),
     Err(E),
 }
+
+// using unwrap_or_else
+fn alternative() {
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+       if error.kind() == ErrorKind::NotFound {
+           File::create("hello.txt").unwrap_or_else(|error| {
+               panic!("Problem creating the file: {:?}", error);
+           })
+       } else {
+           panic!("Problem opening the file: {:?}", error);
+       }
+    });
+}
