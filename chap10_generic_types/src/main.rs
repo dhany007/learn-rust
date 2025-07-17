865eb0d1547c124379bdaea3a0a981b683af4ea5
dhany <dhany@citra-digital.org>
2025-07-17 15:11:52 +0700

    chap10: using generic type

--- chap10_generic_types/src/main.rs
+++ chap10_generic_types/src/main.rs
@@ -2,28 +2,15 @@ fn main() {
     println!("Generic Types");
 
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
 
-
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
+fn largest<T>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
