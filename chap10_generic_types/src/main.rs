c4fc34fea536795af51a1e03ac413ad27e05cc1b
dhany <dhany@citra-digital.org>
2025-07-17 15:10:34 +0700

    chap10: separate parameter

--- chap10_generic_types/src/main.rs
+++ chap10_generic_types/src/main.rs
@@ -1,30 +1,17 @@
 fn main() {
     println!("Generic Types");
-    remove_duplication();
-}
 
-// before
-fn duplication() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let mut largest = &number_list[0];
-    for number in &number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-    println!("The largest number is {}", largest);
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let mut largest = &number_list[0];
-    for number in &number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-    println!("The largest number is {}", largest);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
 
-fn largest(list: &[i32]) -> &i32 {
+
+fn largest_i32(list: &[i32]) -> &i32 {
     let mut largest = &list[0];
 
     for item in list {
@@ -36,13 +23,14 @@ fn largest(list: &[i32]) -> &i32 {
     largest
 }
 
-// after
-fn remove_duplication() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
