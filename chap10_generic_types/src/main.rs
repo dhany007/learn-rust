ec0d6918ab55d00a87f14809e6c7121b0b9adb56
dhany <dhany@citra-digital.org>
2025-07-17 10:51:10 +0700

    chap10_remove_duplication

--- chap10_generic_types/src/main.rs
+++ chap10_generic_types/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Generic Types");
-    duplication();
+    remove_duplication();
 }
 
 // before
@@ -22,4 +22,27 @@ fn duplication() {
         }
     }
     println!("The largest number is {}", largest);
+}
+
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// after
+fn remove_duplication() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }
