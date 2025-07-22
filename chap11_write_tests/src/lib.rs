7a2c4a50b8ea40e7edac9f41a84cae3c98c2c6de
dhany <dhany@citra-digital.org>
2025-07-22 10:25:40 +0700

    chap11: write unit test

---
+++ chap11_write_tests/src/lib.rs
@@ -0,0 +1,105 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle{
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: usize) -> usize {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {name}!")
+}
+
+pub struct Guess{
+    value: i32,
+}
+
+impl Guess{
+    pub fn new(value: i32) -> Guess{
+        if value < 1 || value > 100{
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
+
+        Guess{value}
+    }
+}
+
+pub fn add(x: i32, y: i32) -> i32{
+    x + y
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle{
+            width: 5,
+            height: 1,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle{
+            width: 5,
+            height: 1,
+        };
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    // assert equal
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name(){
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"));
+    }
+
+    // should panic
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100, got 200.")]
+    fn greater_than_100_should_panic() {
+        Guess::new(200);
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100, got 0.")]
+    fn lower_than_1_should_panic() {
+        Guess::new(0);
+    }
+
+    // using Result<T, E>
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(3,4);
+        if result == 7 {
+            Ok(())
+        } else {
+            Err(String::from("three plus four does not equal seven"))
+        }
+    }
+}
