7264044573db8b59fe41dd1a50535859e2b7d112
dhany <dhany@citra-digital.org>
2025-09-10 17:12:38 +0700

    chap17: merging streams

--- chap17_streams_future/src/main.rs
+++ chap17_streams_future/src/main.rs
@@ -26,6 +26,26 @@ fn main() {
                 Err(reason) => println!("Problem: {reason:?}"),
             }
         }
+    });
+
+    println!("Merging streams");
+    trpl::run(async {
+        let messages = get_messages().timeout(Duration::from_millis(200));
+
+        let intervals = get_intervals()
+            .map(|count| format!("Interval: {count}"))
+            .throttle(Duration::from_millis(100))
+            .timeout(Duration::from_secs(10));
+        let merged = messages.merge(intervals).take(20);
+
+        let mut stream = pin!(merged);
+
+        while let Some(result) = stream.next().await {
+            match result {
+                Ok(message) => println!("{message}"),
+                Err(reason) => println!("Problem: {reason:?}"),
+            }
+        }
     })
 }
 
@@ -38,7 +58,30 @@ fn get_messages() -> impl Stream<Item = String> {
             let time_to_sleep = if index % 2 == 0 {100} else {300};
             trpl::sleep(Duration::from_millis(time_to_sleep)).await;
 
-            tx.send(format!("Message: '{message}")).unwrap();
+            if let Err(send_error) = tx.send(format!("Message: '{message}")) {
+                eprintln!("could send message '{message}': {send_error}");
+                break;
+            }
+        }
+    });
+
+    ReceiverStream::new(rx)
+}
+
+fn get_intervals() -> impl Stream<Item = u32> {
+    let (tx, rx) = trpl::channel();
+
+    trpl::spawn_task(async move {
+        let mut count = 0;
+
+        loop {
+            trpl::sleep(Duration::from_millis(1)).await;
+            count += 1;
+
+            if let Err(send_error) = tx.send(count) {
+                eprintln!("could not send interval {count}: {send_error}");
+                break
+            }
         }
     });
 
