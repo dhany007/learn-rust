834043b298bceb67d12c2ceab56677637307a897
dhany <dhany@citra-digital.org>
2025-10-16 10:40:29 +0700

    chap18: state pattern

---
+++ chap18_state_pattern/src/main.rs
@@ -0,0 +1,23 @@
+/*
+The final functionality will look like this:
+- A blog post starts as an empty draft.
+- When the draft is done, a review of the post is requested.
+- When the post is approved, it gets published.
+- Only published blog posts return content to print, so unapproved posts can’t accidentally be published.
+ */
+
+use chap18_state_pattern::Post;
+
+fn main() {
+    println!("The state pattern");
+
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
